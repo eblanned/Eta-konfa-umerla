a8509286df0a6ff843b1ec92e9a24753c2395b08
eblanned <34698473+eblanned@users.noreply.github.com>
2018-01-13 06:01:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,4 +53,4 @@ client.on("message", (message) => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-//client.login("MzkzMTQxMzYyMTYzMzE4Nzk1.DRxc8A.S55fnifVLUs0iN5a9_Fp6EZWBo4");
+
